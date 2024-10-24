96d2ec807339a24adffa56378267c7b5681058c3
HubGaz <diablo0201@wp.pl>
2024-10-24 15:16:17 +0200

    item changes

--- item.cs
+++ item.cs
@@ -1,18 +1,21 @@
-namespace UrMAG{
+namespace UrMag{
 
     class items{
-        public string name{get; set;}
-        public double price{get; set;}
-        public int quantity{get; set;}
+       
+        public int ID{get; set;}
+        public string Name{get; set;}
+        public double Price{get; set;}
+        public int Quantity{get; set;}
 
-        ublic Item(string name) : this(name, 0, 1)
-        {
+        public item(int id, string name) : this(id, name, 0, 1){
+
         }
-        public Item(string name, doublle price) : this(name, price, 1)
-        {   
+        public Item(int id, string name, double price) : this(name, price, 1){   
+
         }
-        public Item(string name, double price, int quantity)
-        {
+        public Item(int id, string name, double price, int quantity){
+
+            ID = id;
             Name = name;
             Price = price;
             Quantity = quantity;
