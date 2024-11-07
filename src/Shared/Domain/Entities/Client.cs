11d7ff2c45e700fe9a02693abbfc43d8b7c5e68a
Hubert Gazda <diablo0201@wp.pl>
2024-11-07 13:05:36 +0100

    chore/foldersort

---
+++ src/Shared/Domain/Entities/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+
+public class Client
+{
+
+    public required int ID { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+
+    public Client(int Id, string name, string lastname)
+    {
+        ID = Id;
+        Name = name;
+        LastName = lastname;
+
+    }
+}
+
+
+
+
---
+++ src/Shared/Domain/Entities/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class item
+{
+    public required int ItemID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    public item(int ItemId, string name, double price, int quantity)
+    {
+        ItemID = ItemId;
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+
+}
+
